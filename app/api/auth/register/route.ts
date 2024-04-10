551d354207b42483f81832845f93bf8b19faf4ff
Renzo Viscio <renzo@gecco.com.mx>
2024-04-09 23:39:30 -0300

    default

--- app/api/auth/register/route.ts
+++ app/api/auth/register/route.ts
@@ -4,7 +4,7 @@ import { NextResponse } from "next/server";
 import bcrypt from "bcrypt";
 import { getServerSession } from "next-auth";
 
-export default async function POST(req: Request) {
+export async function POST(req: Request) {
   try {
     const session = await getServerSession();
     if (!session?.user)
