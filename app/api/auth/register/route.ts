9f9ead18b6c5d61e51f40ebdbe6afb72c6f600e4
Renzo Viscio <renzo@gecco.com.mx>
2024-04-09 22:17:54 -0300

    req res post

--- app/api/auth/register/route.ts
+++ app/api/auth/register/route.ts
@@ -2,16 +2,24 @@ import { getServerSession } from "next-auth/next";
 import { connectToDatabase } from "@/prisma/server-helpers";
 import { prisma } from "@/prisma";
 import { NextResponse } from "next/server";
+import { NextApiRequest, NextApiResponse } from "next";
 import bcrypt from "bcrypt";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { authOptions } from "../[...nextauth]/route";
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  const session = await getServerSession(req, res, authOptions);
+  if (session) {
+    // Signed in
+    console.log("Session", JSON.stringify(session, null, 2));
+  } else {
+    // Not Signed in
+    res.status(401);
+  }
+  res.end();
+};
 
 export const POST = async (req: Request) => {
   try {
-    const session = await getServerSession(authOptions);
-    if (!session) {
-      return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
-    }
-
     const { username, password } = await req.json();
     if (!username || !password)
       return NextResponse.json(
